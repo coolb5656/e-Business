b745b1a32778c3f80ac26d3b113928ed2b033352
coolb5656 <luismata5656@gmail.com>
2022-03-19 23:34:28 -0600

    add products

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,6 @@ def create_app():
     is_prod = os.environ.get('IS_HEROKU', None)
 
     if is_prod:
-        # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://arcmlpkkktnfui:5170d135b04f8b0e8b0daee17b31e690b1d3e70a9b5f3c0a6df5a68f3a01f083@ec2-35-174-56-18.compute-1.amazonaws.com:5432/d3p2h47beia405'
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
         app.config["DEBUG"] = False
     else:
--- app/routes/views/main.py
+++ app/routes/views/main.py
@@ -2,7 +2,7 @@ from fileinput import filename
 import os
 from unicodedata import name
 from flask import Blueprint, render_template, redirect, url_for, request, flash, current_app
-from app.db.models import db, User, Product, Order
+from app.db.models import Club, db, User, Product, Order
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import and_, desc
 from flask_login import login_user, login_required, logout_user
@@ -20,8 +20,10 @@ index
 @main.route('/')
 def index():
     products = [Product()]
-    products = Product.query.order_by(Product.price.desc()).all()
+    products = Product.query.order_by(Product.name.desc()).all()
+    clubs = Club.query.order_by(Club.name.desc()).all()
     for p in products:
         p.price = "$" + str(p.price)
-    featured = products
-    return render_template("main/index.html", products=products, featured=featured)
+    featured = Product.query.order_by(Product.views.desc()).all()
+    featured = featured[:5]
+    return render_template("main/index.html", products=products, featured=featured, clubs=clubs)
